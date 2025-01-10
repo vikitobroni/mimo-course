3c6a9a6bdbc5cb0d91502c7f0a102b66d3a68711
viki00595 <viky00595@gmail.com>
2025-01-10 22:35:32 +0700

    feat(mini-challenges/odd-even-game): update code add looping

--- even-or-odd-game/script.js
+++ even-or-odd-game/script.js
@@ -1,33 +1,44 @@
 const input = require("@mimo-org/input");
+let playerWins = 0;
+let computerWins = 0;
+let rounds = 0;
 
-let playerChoice = input("Choose 'odd' or 'even' :");
-let playerNumber = parseInt(input("Choose a number between 1 and 5: "));
+// Play the game 3 times
+while (rounds < 3) {
+  let playerChoice = input("Choose 'odd' or 'even' :");
+  let playerNumber = parseInt(input("Choose a number between 1 and 5: "));
 
-// Generate a random number between 1 and 5 for the computer
-let computerNumber = Math.floor(Math.random() * 5) + 1;
-let computerChoice = "";
-if (playerChoice === "odd") {
-  computerChoice = "even";
-} else {
-  computerChoice = "odd";
-}
+  // Generate a random number between 1 and 5 for the computer
+  let computerNumber = Math.floor(Math.random() * 5) + 1;
+  let computerChoice = "";
+  if (playerChoice === "odd") {
+    computerChoice = "even";
+  } else {
+    computerChoice = "odd";
+  }
 
-// Calculate the sum of the two numbers and determine if it's odd or even.
-let sum = playerNumber + computerNumber;
-let result = "";
-if (sum % 2 === 0) {
-  result = "even";
-} else {
-  result = "odd";
-}
+  // Calculate the sum of the two numbers and determine if it's odd or even.
+  let sum = playerNumber + computerNumber;
+  let result = "";
+  if (sum % 2 === 0) {
+    result = "even";
+  } else {
+    result = "odd";
+  }
 
-// Display the result
-console.log(`Player chose ${playerNumber} (${playerChoice})`);
-console.log(`Computer chose ${computerNumber} (${computerChoice})`);
-console.log(`The sum of the numbers is ${sum}, which is ${result}`);
+  // Display the result
+  console.log(`Player chose ${playerNumber} (${playerChoice})`);
+  console.log(`Computer chose ${computerNumber} (${computerChoice})`);
+  console.log(`The sum of the numbers is ${sum}, which is ${result}`);
 
-if (result === playerChoice) {
-  console.log("Player wins");
-} else {
-  console.log("Computer wins");
+  // Increment the rounds
+  rounds++;
+
+  if (result === playerChoice) {
+    console.log("Player wins by " + rounds);
+    playerWins++;
+  } else {
+    console.log("Computer wins by " + rounds);
+    computerWins++;
+  }
 }
